edf6bbf9a68e2ee65c089b57714c26f2d1b6e143
FonsahPageo <ashprincepageo@gmail.com>
2026-05-05 12:11:57 +0100

    set up pipelines for staging deployments

--- baotechnologies-staging/modules/dns/main.tf
+++ baotechnologies-staging/modules/dns/main.tf
@@ -46,3 +46,35 @@ resource "google_dns_record_set" "www_staging" {
   managed_zone = google_dns_managed_zone.prod.name
   rrdatas      = ["staging.${var.prod_domain}."]
 }
+
+resource "google_dns_record_set" "resend_dkim" {
+  name         = "resend._domainkey.${var.prod_domain}."
+  type         = "TXT"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.prod.name
+  rrdatas      = ["p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDWeuliiBsd1xWwx80h6Jy6MvEd8Ou4SGyhdc/RDlfL0T7OSEuCuol61qYqVGMrr7ARe42SoIABRMPr0DwZScIjC6U1Sg+a6MfrOdZkMxnID2KGqknvJryBdsHirv4T0qNqLoN/JTkemiLp/1UEfOH7GEL+LfDdrDh4N/oFaas87wIDAQAB"]
+}
+
+resource "google_dns_record_set" "resend_mx" {
+  name         = "send.${var.prod_domain}."
+  type         = "MX"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.prod.name
+  rrdatas      = ["10 feedback-smtp.us-east-1.amazonses.com."]
+}
+
+resource "google_dns_record_set" "resend_txt" {
+  name         = "send.${var.prod_domain}."
+  type         = "TXT"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.prod.name
+  rrdatas      = ["v=spf1 include:amazonses.com ~all"]
+}
+
+resource "google_dns_record_set" "resend_dmarc" {
+  name         = "_dmarc.${var.prod_domain}."
+  type         = "TXT"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.prod.name
+  rrdatas      = ["v=DMARC1; p=none;"]
+}
